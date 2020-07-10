d147338b7a8f0e8d28cd0f2d31b58f6f86d97e14
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-07-10 19:47:04 -0300

    Fix migrations

--- applications/users/migrations/0001_initial.py
+++ applications/users/migrations/0001_initial.py
@@ -27,8 +27,6 @@ class Migration(migrations.Migration):
                 ('is_superuser', models.BooleanField(default=False)),
                 ('is_active', models.BooleanField(default=True)),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission', verbose_name='user permissions')),
             ],
             options={
                 'abstract': False,
