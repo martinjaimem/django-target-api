0e726578cb23b6189fd8380d7d8cdc43c212341a
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-07-23 17:58:44 -0300

    Refactor log out test to check if it worked

--- applications/users/tests/test_log_out.py
+++ applications/users/tests/test_log_out.py
@@ -1,3 +1,4 @@
+from rest_framework.authtoken.models import Token
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 from rest_framework.views import status
@@ -17,10 +18,14 @@ class UserLogOutTests(APITestCase):
         self.user = UserFactory(confirmed=True)
 
     def test_valid_tokenf_respond_success(self):
-        self.client.force_authenticate(user=self.user)
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         response = self.call_log_out()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.get(reverse('rest_user_details'))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_invalid_token_respond_unauthorized(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token fakeToken')
