552243d05761594471f55cecf7f1c63a6adf877c
Agustin Martinez <agustin.martinez@rootstrap.com>
2020-07-29 15:13:20 -0300

    Add admin panel of Contact Admin

--- applications/contacts/admin.py
+++ applications/contacts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('email', 'writer', 'short_message', 'created_at',)
+    list_filter = ('created_at',)
+    search_fields = ('email', 'message',)
+    fields = ('email', 'writer', 'message', 'created_at', 'updated_at',)
+
+    def short_message(self, obj):
+        return f'{obj.message[:60]}...' if len(obj.message) > 60 else obj.message
+
+    short_message.short_description = 'message'
+
+    def has_add_permission(self, *args):
+        return False
+
+    def has_change_permission(self, *args):
+        return False
+
+
+admin.site.register(Contact, ContactAdmin)
